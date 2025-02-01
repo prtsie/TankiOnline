fef02db88c17801f9019efc367c3a354f39100fc
prtsie <yonhsup@mail.ru>
2025-02-01 11:16:11 +0300

    sixth

--- app/src/main/java/com/example/tankionline/drawers/ElementsDrawer.kt
+++ app/src/main/java/com/example/tankionline/drawers/ElementsDrawer.kt
@@ -19,6 +19,36 @@ class ElementsDrawer(val container: FrameLayout) {
         val topMargin = y.toInt() - (y.toInt() % CELL_SIZE)
         val leftMargin = x.toInt() - (x.toInt() % CELL_SIZE)
         val coordinate = Coordinate(topMargin, leftMargin)
+        if (currentMaterial == Material.EMPTY) {
+            eraseView(coordinate)
+        } else {
+            drawOrReplaceView(coordinate)
+        }
+    }
+
+    private fun drawOrReplaceView(coordinate: Coordinate) {
+        val viewOnCoordinate = getElementByCoordinates(coordinate)
+        if (viewOnCoordinate == null) {
+            drawView(coordinate)
+            return
+        }
+        if (viewOnCoordinate.material != currentMaterial) {
+            replaceView(coordinate)
+        }
+    }
+
+    private fun eraseView(coordinate: Coordinate) {
+        val elemetOnCoordinate = getElementByCoordinates(coordinate)
+        if (elemetOnCoordinate != null) {
+            val erasingView = container.findViewById<View>(elemetOnCoordinate.viewId)
+            container.removeView(erasingView)
+            elementsOnContainer.remove(elemetOnCoordinate)
+        }
+    }
+
+    private fun replaceView(coordinate: Coordinate) {
+        eraseView(coordinate)
+        drawView(coordinate)
     }
 
     fun drawView(coordinate: Coordinate) {

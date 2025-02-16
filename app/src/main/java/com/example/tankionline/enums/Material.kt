3a79c441c6178ebf3e72149c58509f7e8c3f8be8
prtsie <yonhsup@mail.ru>
2025-02-16 23:41:34 +0300

    четырнадцатая

--- app/src/main/java/com/example/tankionline/enums/Material.kt
+++ app/src/main/java/com/example/tankionline/enums/Material.kt
@@ -5,18 +5,21 @@ import com.example.tankionline.R
 const val CELLS_SIMPLE_ELEMENT = 1
 const val CELLS_EAGLE_WIDTH = 4
 const val CELLS_EAGLE_HEIGHT = 3
+const val CELLS_TANK_SIZE = 2
 
 enum class Material(
     val tankCanGoThrough: Boolean,
     val bulletCanGoThrough: Boolean,
     val simpleBulletCanDestroy: Boolean,
-    val canExistOnlyOne: Boolean,
+    val elementsAmountOnScreen: Int,
     val width: Int,
     val height: Int,
     val image: Int) {
-    EMPTY(true, true, true, false, 0, 0, 0),
-    BRICK(false, false, true, false, CELLS_SIMPLE_ELEMENT, CELLS_SIMPLE_ELEMENT, R.drawable.brick),
-    CONCRETE(false, false, false, false, CELLS_SIMPLE_ELEMENT, CELLS_SIMPLE_ELEMENT, R.drawable.concrete),
-    GRASS(true, true, false, false, CELLS_SIMPLE_ELEMENT, CELLS_SIMPLE_ELEMENT, R.drawable.grass),
-    EAGLE(false, false, true, true, CELLS_EAGLE_WIDTH, CELLS_EAGLE_HEIGHT, R.drawable.eagle)
+    EMPTY(true, true, true, 0, 0, 0, 0),
+    BRICK(false, false, true, 0, CELLS_SIMPLE_ELEMENT, CELLS_SIMPLE_ELEMENT, R.drawable.brick),
+    CONCRETE(false, false, false, 0, CELLS_SIMPLE_ELEMENT, CELLS_SIMPLE_ELEMENT, R.drawable.concrete),
+    GRASS(true, true, false, 0, CELLS_SIMPLE_ELEMENT, CELLS_SIMPLE_ELEMENT, R.drawable.grass),
+    EAGLE(false, false, true, 1, CELLS_EAGLE_WIDTH, CELLS_EAGLE_HEIGHT, R.drawable.eagle),
+    ENEMY_TANK_RESPAWN(false, false, true, 3, CELLS_TANK_SIZE, CELLS_TANK_SIZE, R.drawable.enemy_tank),
+    PLAYER_TANK_RESPAWN(false, false, true, 1, CELLS_TANK_SIZE, CELLS_TANK_SIZE, R.drawable.tank)
 }

331c7a5fac833d2532bc7ad4185eb9a67b7ebc5b
prtsie <yonhsup@mail.ru>
2025-02-16 23:21:30 +0300

    тринадцатая

--- app/src/main/java/com/example/tankionline/models/Element.kt
+++ app/src/main/java/com/example/tankionline/models/Element.kt
@@ -1,9 +1,10 @@
 package com.example.tankionline.models
 
+import android.view.View
 import com.example.tankionline.enums.Material
 
-data class Element(
-    val viewId: Int,
+data class Element constructor(
+    val viewId: Int = View.generateViewId(),
     val material: Material,
     val coordinate: Coordinate,
     val width: Int,
